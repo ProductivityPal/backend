19da1d8fd8d598f55ef61f86d2022d19c2311937
Ewelaiam <ewelinaflisak@gmail.com>
2023-12-11 22:33:59 +0100

    add student for presentation

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -14,6 +14,8 @@ BEGIN
              values (4, ''TimW12'', ''$2a$10$/nL8rFQ4w6ZOVU8wBbSTFeJS28NE15P7jOo/OFY7S6nrFoNWQazAC'', ''timmit@gmail.com'', ''LOW'', false, ''USER'');
             insert into app_user(id, username, password, email, current_energy_level, is_email_verified, role)
             values (5, ''student'', ''$2a$10$/nL8rFQ4w6ZOVU8wBbSTFeJS28NE15P7jOo/OFY7S6nrFoNWQazAC'', ''student@gmail.com'', ''MEDIUM'', false, ''USER'');
+            insert into app_user(id, username, password, email, current_energy_level, is_email_verified, role)
+            values (6, ''student2'', ''$2a$10$/nL8rFQ4w6ZOVU8wBbSTFeJS28NE15P7jOo/OFY7S6nrFoNWQazAC'', ''student2@gmail.com'', ''MEDIUM'', false, ''USER'');
     END IF;
 
     IF (NOT EXISTS(SELECT * FROM task))
@@ -79,6 +81,39 @@ BEGIN
 
             INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id)
             VALUES (22, ''Group project meeting'', ''Coordinate with group members and schedule a meeting to work on the group project'', 4, ''EASY'', ''LOVE'', ''2023-06-08'', 1, NULL, false, false, false, NULL, 5);
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id)
+            VALUES (23, ''Group project meeting'', ''Coordinate with group members and schedule a meeting to work on the group project'', 4, ''EASY'', ''LOVE'', ''2023-06-08'', 1, NULL, false, false, false, NULL, 5);
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (24, ''Code review for feature A'', ''Review and provide feedback on the code for feature A'', 3, ''MEDIUM'', ''NEUTRAL'', ''2023-12-08'', 60, 73, false, false, true, NULL, 6, ''beige'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (25, ''Prepare presentation slides'', ''Create slides for the upcoming project presentation'', 2, ''EASY'', ''DISLIKE'', ''2023-12-07'', 30, 55, false, false, true, NULL, 6, ''green'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (26, ''Database schema design'', ''Work on designing the database schema for the new application'', 4, ''MEDIUM'', ''LIKE'', ''2023-11-30'', 45, 40, false, false, true, NULL, 6, ''accent'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (27, ''Write unit tests'', ''Create unit tests for the key components of the application'', 3, ''MEDIUM'', ''HATE'', ''2023-12-08'', 50, NULL, false, false, false, NULL, 6, ''grey'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (28, ''Project documentation'', ''Document the progress and key decisions made during the project'', 2, ''EASY'', ''LIKE'', ''2023-12-10'', 90, NULL, false, false, false, NULL, 6, ''beige'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (29, ''Bug fixing for release'', ''Identify and fix any bugs before the upcoming release'', 4, ''HARD'', ''LOVE'', ''2023-12-15'', 30, NULL, false, false, false, NULL, 6, ''green'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (30, ''Client meeting preparation'', ''Prepare for the client meeting to discuss project requirements'', 3, ''EXTRA_HARD'', ''DISLIKE'', ''2023-12-30'', 20, NULL, false, false, false, NULL, 6, ''beige'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (31, ''Security audit'', ''Conduct a security audit for the application code and infrastructure'', 4, ''HARD'', ''LOVE'', ''2024-01-11'', 50, NULL, false, false, false, NULL, 6, ''green'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (32, ''User training materials'', ''Develop training materials for end-users of the application'', 2, ''EASY'', ''HATE'', ''2024-12-20'', 40, NULL, false, false, false, NULL, 6, ''grey'');
+
+            INSERT INTO task(id, name, description, priority, difficulty, likeliness, deadline, time_estimate, completion_time, is_subtask, is_parent, is_completed, parent_id, app_user_id, category)
+            VALUES (33, ''Final project review'', ''Conduct a final review of the entire project and make any necessary adjustments'', 3, ''MEDIUM'', ''LOVE'', ''2024-01-02'', 60, NULL, false, false, false, NULL, 6, ''grey'');
         END IF;
 
     IF (NOT EXISTS(SELECT * FROM calendar))
@@ -93,6 +128,8 @@ BEGIN
             values (4, ''Default'', 4);
             insert into calendar(id, name, app_user_id)
             values (5, ''Default'', 5);
+            insert into calendar(id, name, app_user_id)
+            values (6, ''Default'', 6);
         END IF;
 
     IF (NOT EXISTS(SELECT * FROM calendar_task))
@@ -107,6 +144,8 @@ BEGIN
             values (''2019-11-08'', null, 3, 4);
             insert into calendar_task(start_date, end_date, calendar_id, task_id)
             values (''2019-09-08'', null, 1, 5);
+            insert into calendar_task(start_date, end_date, calendar_id, task_id)
+            values (''2023-12-14'', null, 6, 33);
         END IF;
 
     IF (NOT EXISTS(SELECT * FROM category))
@@ -119,6 +158,24 @@ BEGIN
             values (''accent'', 5);
             insert into category(default_name, app_user_id)
             values (''grey'', 5);
+            insert into category(default_name, app_user_id)
+            values (''beige'', 6);
+            insert into category(default_name, app_user_id)
+            values (''green'', 6);
+            insert into category(default_name, app_user_id)
+            values (''accent'', 6);
+            insert into category(default_name, app_user_id)
+            values (''grey'', 6);
+        END IF;
+
+    IF (NOT EXISTS(SELECT * FROM energy_level_info))
+        THEN
+            insert into energy_level_info(energy_level, notification_time, app_user_id)
+            values (''LOW'', ''2023-12-10T11:59:11.332Z'', 6);
+            insert into energy_level_info(energy_level, notification_time, app_user_id)
+            values (''LOW'', ''2023-12-11T11:59:11.332Z'', 6);
+            insert into energy_level_info(energy_level, notification_time, app_user_id)
+            values (''MEDIUM'', ''2023-12-11T08:59:11.332Z'', 6);
         END IF;
 
 
@@ -135,5 +192,6 @@ begin
     tmp = (SELECT setval(''category_id_seq'', (SELECT MAX(id) FROM category)));
     tmp = (SELECT setval(''calendar_id_seq'', (SELECT MAX(id) FROM calendar)));
     tmp = (SELECT setval(''calendar_task_id_seq'', (SELECT MAX(id) FROM calendar_task)));
+    tmp = (SELECT setval(''energy_level_info_id_seq'', (SELECT MAX(id) FROM energy_level_info)));
 end;
 '  LANGUAGE PLPGSQL;
